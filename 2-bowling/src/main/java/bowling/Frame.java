fc137f6dd6bb70b3bbb7a8b3b01e96210fca91ba
Pablo Ponzio <pponzio@dc.exa.unrc.edu.ar>
2022-08-30 15:23:55 -0300

    Advancing the bowling implementation

--- 2-bowling/src/main/java/bowling/Frame.java
+++ 2-bowling/src/main/java/bowling/Frame.java
@@ -2,19 +2,55 @@ package bowling;
 
 public class Frame {
 	
-	private int score;
+	private int first;
+	private int second;
+	private int numRolls;
+	Frame next;
 
 	public void registerRoll(int pins) {
-		if (pins < 0 || pins > 10)
+		if (invalidNumberOfPins(pins))
 			throw new IllegalArgumentException();
-		if (score + pins > 10)
+		if (invalidResultingScore(pins))
 			throw new IllegalArgumentException();
+		
+		registerCurrentRoll(pins);
+	}
 
-		score += pins;
+	private void registerCurrentRoll(int pins) {
+		switch (numRolls) {
+		case 0:
+			first = pins;
+			break;
+		case 1:
+			second = pins;
+			break;
+		default:
+			throw new IllegalStateException(); 
+		}
+		numRolls++;
+	}
+
+	private boolean invalidResultingScore(int pins) {
+		return first + second + pins > 10;
+	}
+
+	private boolean invalidNumberOfPins(int pins) {
+		return pins < 0 || pins > 10;
 	}
 
 	public int score() {
-		return score;
+		int partialScore = first + second;
+		if (first == 10)
+			return partialScore + next.first + next.second;
+		
+		if (partialScore == 10) 
+			return partialScore + next.first;
+		
+		return partialScore;
+	}
+
+	public void setNext(Frame next) {
+		this.next = next;
 	}
 
 }
--- 2-bowling/src/test/java/bowling/FrameTest.java
+++ 2-bowling/src/test/java/bowling/FrameTest.java
@@ -44,4 +44,53 @@ public class FrameTest {
 				() -> f.registerRoll(6));
 	}
 	
+	@Test
+	void testRegisterThreeRolls() {
+		Frame f = new Frame();
+		f.registerRoll(3);
+		f.registerRoll(3);
+		assertThrows(IllegalStateException.class,
+				() -> f.registerRoll(3));
+	}
+	
+	@Test
+	void testRegisterSpare() {
+		Frame f1 = new Frame();
+		Frame f2 = new Frame();
+		f1.setNext(f2);
+
+		f1.registerRoll(5);
+		f1.registerRoll(5);
+		assertEquals(10, f1.score());
+
+		f2.registerRoll(5);
+		assertEquals(5, f2.score());
+		assertEquals(15, f1.score());
+	}
+	
+	
+	@Test
+	void testRegisterStrike() {
+		Frame f1 = new Frame();
+		Frame f2 = new Frame();
+		f1.setNext(f2);
+
+		f1.registerRoll(10);
+		assertEquals(10, f1.score());
+
+		f2.registerRoll(4);
+		f2.registerRoll(3);
+		assertEquals(7, f2.score());
+		assertEquals(17, f1.score());
+	}
+	
+	@Test
+	void testRegisterStrikeAndZero() {
+		Frame f = new Frame();
+		f.registerRoll(10);
+		assertThrows(IllegalStateException.class,
+				() -> f.registerRoll(0));
+	}
+	
+	
 }
